3995acd0aafaa11418d792eb9ea4991c3763e78b
Vina Myrnauli Abigail Siallagan <120386690+vinamyrnauli@users.noreply.github.com>
2023-09-16 13:04:55 +0700

    Selesai Week 2

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -16,7 +16,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 # ####################### Replace REC2 ####
-REC2="80CA4E414A5D189E7661699AF788A63BB62C93A5"
+REC2="F788A63BB62C93A5"
 # ####################### ####### #### ####
 # REC1: public key
 REC1="63FB12B215403B20"
